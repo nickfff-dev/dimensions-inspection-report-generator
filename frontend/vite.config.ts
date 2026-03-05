30201f02d19077e3fd2f0aa8c8966f168f44857b
nickfff-dev <hunchographics@gmail.com>
2026-03-06 01:27:26 +0300

    allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     port: 3000,
+    allowedHosts:['bashaer-qaqc.top', 'localhost'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
@@ -13,4 +15,5 @@ export default defineConfig({
       },
     },
   },
+  
 })
